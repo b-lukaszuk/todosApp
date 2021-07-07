a70e2a3848cb03e1cead44fb6aaad5b57dc1c299
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-07-07 21:29:11 +0200

    info o local storage i json placeholder w landPage + linki

--- my-app/src/conteners/LandingPage.js
+++ my-app/src/conteners/LandingPage.js
@@ -54,8 +54,33 @@ function LandingPage(props) {
                 </div>
             </div>
             <div className="container">
-                <Button className="normalBut"
-                    onClick={handleLogin} btnText="Log In" />
+                <Button className="normalBut" onClick={handleLogin} btnText="Log In" />
+            </div>
+            <div class="centered-div">
+                <p>Please be informed that:</p>
+                <ul>
+                    <li>
+                        once You are logged-in the page may dowload mock todos from{" "}
+                        <a href="https://jsonplaceholder.typicode.com/users/1/todos">
+                            This web page
+                        </a>
+                    </li>
+                    <li>
+                        once You are logged-in the page may read todos from{" "}
+                        <a href="https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage/">
+                            localStorage
+                        </a>
+                    </li>
+                    <li>
+                        once You are logged-in the page will store the todos in{" "}
+                        <a href="https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage/">
+                            localStorage
+                        </a>
+                    </li>
+                </ul>
+                <p>
+                    If You don't want any of this to happen then don't log in and leave the page
+                </p>
             </div>
         </div>
     );
